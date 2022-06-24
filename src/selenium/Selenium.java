d970f1dd98af0bbb46d8c17f9aaaac8c78359176
Net <lethuhuong021@gmail.com>
2022-06-25 01:04:36 +0700

    Selenium Webdriver

--- src/selenium/Selenium.java
+++ src/selenium/Selenium.java
@@ -21,18 +21,19 @@ public class Selenium {
      */
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
+        // khởi tạo môi trường test và khởi chạy trang web cần test
             System.setProperty("webdriver.chrome.driver","C:\\Users\\MM\\Documents\\NetBeansProjects\\Selenium\\chromedriver.exe");
             String url="http://127.0.0.1:8000/login";
             WebDriver driver = new ChromeDriver();
             driver.get(url);
-            
+        //  maximize trang web  
             driver.manage().window().maximize();
             Thread.sleep(1000);
             
             WebElement btn_login = driver.findElement(By.xpath("//header/div[1]/div[1]/div[1]/div[1]/a[1]"));
             btn_login.click();
             Thread.sleep(3000);
-               
+        //  xác định 2 dòng input bằng name       
             WebElement name_email = driver.findElement(By.name("username"));
             name_email.sendKeys("huong@gmail.com");
             Thread.sleep(1000);
@@ -40,9 +41,10 @@ public class Selenium {
             WebElement name_password = driver.findElement(By.name("password"));
             name_password.sendKeys("123456");
             Thread.sleep(1000);
-                
+        // xác định dòng button đăng nhập bằng xpath       
             WebElement btn_dangnhap = driver.findElement(By.xpath("//body/div[@id='tg-wrapper']/main[@id='tg-main']/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/button[1]"));
-            btn_dangnhap.click(); 
+            btn_dangnhap.click();
+        // lấy tra title khi đăng nhập thành công để làm điều kiện test pass hay 
             String at = driver.getTitle();
             String et = "BookStore";
             driver.close();
